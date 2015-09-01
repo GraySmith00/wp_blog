547b32481f8f2c3e54383b098a7cfc35fc0d4eb5
Gray Smith <gray.smith00@gmail.com>
2015-09-01 16:22:26 +0000

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def blog
+  end
+
+  def resources
+  end
+
+  def contact
+  end
+  
+  def course features
+  end
+  
+  
+end
